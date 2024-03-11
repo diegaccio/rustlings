165bf19c44b6b2b4692f69b49f8623237d625c2e
diegaccio <dcattaneo@gmail.com>
2024-03-11 12:22:29 +0100

    till semantics 3

--- exercises/move_semantics/move_semantics1.rs
+++ exercises/move_semantics/move_semantics1.rs
@@ -3,19 +3,23 @@
 // Execute `rustlings hint move_semantics1` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
 
 #[test]
 fn main() {
     let vec0 = vec![22, 44, 66];
 
-    let vec1 = fill_vec(vec0);
+    //per riutilizzarlo dopo devo clonare vec0, altrimenti viene ownato dall'altra funzione
+    let vec1 = fill_vec(vec0.clone());
+    
+    let vec2 = fill_vec(vec0);
+
 
     assert_eq!(vec1, vec![22, 44, 66, 88]);
+    assert_eq!(vec2, vec![22, 44, 66, 88]);
 }
 
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let vec = vec;
+    let mut vec = vec;
 
     vec.push(88);
 

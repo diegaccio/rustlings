dbe3a29c58c5b1c092154e4ad214e8919418bb93
diegaccio <dcattaneo@gmail.com>
2024-03-11 08:47:46 +0100

    primitive types

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -5,10 +5,12 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
+
 
 fn main() {
-    let a = ???
+    
+    //101 elementi inizializzati a 0
+    let a = [0; 101];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
